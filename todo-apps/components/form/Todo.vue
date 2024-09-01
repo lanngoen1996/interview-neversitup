ada53e915f9c22469ffa5aa80b01eb6414c83f8e
noppadol_la <noppadol_la@gmail.com>
2024-09-02 01:09:31 +0700

    feat: create api create

--- todo-apps/components/form/Todo.vue
+++ todo-apps/components/form/Todo.vue
@@ -4,6 +4,7 @@ import { DatePicker } from 'v-calendar'
 import 'v-calendar/dist/style.css'
 import BtnIconMini from '../button/BtnIconMini.vue'
 import dayjs from 'dayjs'
+import type { TodoResponse } from '~/types/Todo'
 
 // Message page
 const message = {
@@ -62,6 +63,23 @@ const layoutStore = useLayoutStore()
 
 const date = ref(dayjs().toDate())
 
+const form = ref({
+  title: '',
+  description: ''
+})
+
+const create = async () => {
+  try {   
+    await useAPI<{ data: TodoResponse }>('/todo', {
+      method: 'POST',
+      body: form.value
+    })
+    layoutStore.toggleForm()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 </script>
 
 <template>
@@ -74,76 +92,90 @@ const date = ref(dayjs().toDate())
       />
       <p class="title pl-5">{{  message.title }}</p>
     </div>
-    <div class="px-5">
-      <div>
-        <p class="base text-gray-500 font-semibold">
-          {{ message.form.icon.title }}
-        </p>
-        <div class="mt-5">
-          <div class="flex justify-between">
-            <BtnIconMini v-for="(item, index) in message.form.icon.list" :key="index" :bg="item.color" :position="''" class="mr-2"> 
-              <UIcon 
-                :name="item.icon" 
-                class="h-7 w-7 text-white"
-              />
-            </BtnIconMini>
+    <form @submit.prevent="create">
+      <div class="px-5">
+        <div>
+          <p class="base text-gray-500 font-semibold">
+            {{ message.form.icon.title }}
+          </p>
+          <div class="mt-5">
+            <div class="flex justify-between">
+              <BtnIconMini v-for="(item, index) in message.form.icon.list" :key="index" :bg="item.color" :position="''" class="mr-2"> 
+                <UIcon 
+                  :name="item.icon" 
+                  class="h-7 w-7 text-white"
+                />
+              </BtnIconMini>
+            </div>
           </div>
         </div>
+        <div class="mt-10">
+          <p class="base text-gray-500 font-semibold">
+            {{ message.form.name.title }}
+          </p>
+          <input 
+            v-model="form.title" 
+            :placeholder="message.form.name.placeholder"
+            type="text"
+            name="title"
+            class="mt-3 w-full h-10 border-b-2 border-indigo-200 border-b-indigo-500 rounded-t-md focus:outline-none text-md px-2"
+          >
+        </div>
+        <div class="mt-5">
+          <p class="base text-gray-500 font-semibold">
+            {{ message.form.description.title }}
+          </p>
+          <textarea 
+            v-model="form.description" 
+            name="description" 
+            :placeholder="message.form.description.placeholder" 
+            rows="5" 
+            class="w-full border-2 rounded mt-3 p-2 focus:outline-none" 
+          />
+        </div>
+        <div class="mt-5">
+          <p class="base text-gray-500 font-semibold">
+            {{ message.form.date.title }}
+          </p>
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <input 
+              :placeholder="message.form.date.placeholder" 
+              type="text"
+              name="date"
+              class="mt-3 w-full h-10 border-b-2 border-indigo-200 border-b-indigo-500 rounded-t-md focus:outline-none text-md px-2"
+              :value="`${dayjs(date).format('DD - MMMM - YYYY')}`"
+            >
+            <template #panel="{ close }">
+              <DatePicker v-model="date" mode="date"  @close="close" />
+            </template>
+          </UPopover>
+        </div>
+        <div class="mt-5">
+          <p class="base text-gray-500 font-semibold">
+            {{ message.form.time.title }}
+          </p>
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <input 
+              :placeholder="message.form.time.placeholder" 
+              type="text" 
+              name="time"
+              class="mt-3 w-full h-10 border-b-2 border-indigo-200 border-b-indigo-500 rounded-t-md focus:outline-none text-md px-2"
+              :value="`${dayjs(date).format('hh : mm A')}`"
+            >
+            <template #panel="{ close }">
+              <DatePicker v-model="date" mode="time"  @close="close" />
+            </template>
+          </UPopover>
+        </div>
       </div>
-      <div class="mt-10">
-        <p class="base text-gray-500 font-semibold">
-          {{ message.form.name.title }}
-        </p>
-        <input 
-          :placeholder="message.form.name.placeholder" 
-          type="text"
-          class="mt-3 w-full h-10 border-b-2 border-indigo-200 border-b-indigo-500 rounded-t-md focus:outline-none text-md px-2"
+      <div class="px-5 mt-40">
+        <button 
+          type="submit" 
+          class="bg-gradient-to-r from-blue-700 to-sky-400 px-12 py-2 text-white rounded-full hover:-translate-y-0.5 hover:-translate-x-0.5 hover:shadow-lg duration-200"
         >
+          Add
+        </button>
       </div>
-      <div class="mt-5">
-        <p class="base text-gray-500 font-semibold">
-          {{ message.form.description.title }}
-        </p>
-        <textarea :placeholder="message.form.description.placeholder" rows="5" class="w-full border-2 rounded mt-3 p-2 focus:outline-none" />
-      </div>
-      <div class="mt-5">
-        <p class="base text-gray-500 font-semibold">
-          {{ message.form.date.title }}
-        </p>
-        <UPopover :popper="{ placement: 'bottom-start' }">
-          <input 
-            :placeholder="message.form.date.placeholder" 
-            type="text" 
-            class="mt-3 w-full h-10 border-b-2 border-indigo-200 border-b-indigo-500 rounded-t-md focus:outline-none text-md px-2"
-            :value="`${dayjs(date).format('DD - MMMM - YYYY')}`"
-          >
-          <template #panel="{ close }">
-            <DatePicker v-model="date" mode="date"  @close="close" />
-          </template>
-        </UPopover>
-      </div>
-      <div class="mt-5">
-        <p class="base text-gray-500 font-semibold">
-          {{ message.form.time.title }}
-        </p>
-        <UPopover :popper="{ placement: 'bottom-start' }">
-          <input 
-            :placeholder="message.form.time.placeholder" 
-            type="text" 
-            class="mt-3 w-full h-10 border-b-2 border-indigo-200 border-b-indigo-500 rounded-t-md focus:outline-none text-md px-2"
-            :value="`${dayjs(date).format('hh : mm A')}`"
-          >
-          <template #panel="{ close }">
-            <DatePicker v-model="date" mode="time"  @close="close" />
-          </template>
-        </UPopover>
-      </div>
-    </div>
-    <div class="px-5 mt-40">
-      <button 
-        type="button" 
-        class="bg-gradient-to-r from-blue-700 to-sky-400 px-12 py-2 text-white rounded-full hover:-translate-y-0.5 hover:-translate-x-0.5 hover:shadow-lg duration-200"
-      >Add</button>
-    </div>
+    </form>
   </div>
 </template>
