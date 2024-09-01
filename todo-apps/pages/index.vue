92498fc6aa4d4ba60b386cacf888cac9b42e1018
noppadol_la <noppadol_la@gmail.com>
2024-09-02 01:32:26 +0700

    feat: create model

--- todo-apps/pages/index.vue
+++ todo-apps/pages/index.vue
@@ -21,13 +21,33 @@ const all = await useAPI<{ data: TodoResponse[] }>('/todo').then((resp) => {
   return []
 })
 
+const isOpen = ref(false)
+const display = ref({
+  title: '',
+  description: ''
+})
+
+const onModal = (item: TodoSanity) => {
+  isOpen.value = !isOpen.value
+  display.value = item
+}
+
 </script>
 
 <template>
   <div>
-    <TodoCard v-for="(item, index) in all" :key="index" :message="item.description" :date="item.date">
+    <TodoCard  
+      v-for="(item, index) in all" 
+      :key="index" 
+      :message="item.description" 
+      :date="item.date" 
+      @click="onModal(item)"
+    >
       <template #icon>
-        <BtnIconMini bg="bg-gradient-to-r from-purple-500 to-pink-500" class="mr-2"> 
+        <BtnIconMini 
+          bg="bg-gradient-to-r from-purple-500 to-pink-500" 
+          class="mr-2" 
+        > 
           <UIcon 
             name="i-fluent-emoji-high-contrast-cat-face" 
             class="h-7 w-7 text-white"
@@ -35,5 +55,18 @@ const all = await useAPI<{ data: TodoResponse[] }>('/todo').then((resp) => {
         </BtnIconMini>
       </template>
     </TodoCard>
+
+    <UModal
+      v-model="isOpen" 
+      :ui="{
+        base: 'w-[385px]',
+        container: 'flex min-h-full items-end sm:items-start justify-center text-center'
+      }
+    ">
+      <div class="p-4">
+        <p class="title mb-3">{{ display.title }}</p>
+        <span>{{ display.description }}</span>
+      </div>
+    </UModal>
   </div>
 </template>
